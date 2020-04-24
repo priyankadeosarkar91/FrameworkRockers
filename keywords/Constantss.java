ae6cfe8dc4b3abbbc4fdb8ef40c1075855b7fba5
PiyuPatil29 <piyunish91@gmail.com>
2020-04-24 22:36:48 +0530

    Keywords commit

---
+++ keywords/Constantss.java
@@ -0,0 +1,15 @@
+package com.keywords;
+	import java.io.FileInputStream;
+	import java.util.Properties;
+
+	import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.interactions.Actions;
+
+	public class Constantss {
+		public static WebDriver driver;
+		public static FileInputStream file;
+		public static Properties prop;
+		public static Actions action;
+	}
+
+
